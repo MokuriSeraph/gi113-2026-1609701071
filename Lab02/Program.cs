9874a0cd367a0ef53231a8c9dd4e8f268ad2715f
Mokuri <kiaw2973@gmail.com>
2026-08-30 00:10:41 +0700

    Update Program.cs

--- Lab02/Program.cs
+++ Lab02/Program.cs
@@ -1,10 +1,19 @@
-﻿namespace Lab02
+﻿/*
+ * Student ID : 1690701071
+ * Name       : Peraphat Sungwan
+ * Section    : 129A
+ * No.        : N/A
+ * Course     : GI113 Computer Programming (GI)
+ */
+
+
+namespace Lab02
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Test10101010");
         }
     }
 }
